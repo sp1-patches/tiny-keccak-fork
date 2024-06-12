7891dc04f4a6f90bafefad240ded4d2e30b44838
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-06-12 15:23:20 -0700

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -147,7 +147,15 @@ mod keccakf;
     feature = "tuple_hash",
     feature = "parallel_hash"
 ))]
-pub use keccakf::keccakf;
+
+cfg_if::cfg_if! {
+    if #[cfg(all(target_os = "zkvm", target_vendor = "succinct", target_arch = "riscv32"))] {
+        mod succinct;
+        pub use succinct::keccakf;
+    } else {
+        pub use keccakf::keccakf;
+    }
+}
 
 #[cfg(feature = "k12")]
 mod k12;
